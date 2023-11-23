abe0f988d6718eafb886eea8ec8d95562ea3c991
lewiswolf <lewiswolstanholme@gmail.com>
2023-11-23 19:51:17 +0000

    refactored and changed for loop to begin from 1

--- code/src/index.ts
+++ code/src/index.ts
@@ -58,50 +58,38 @@ maxmsp.addHandler('setRange', (f_min: Readonly<number> = 0, f_max: Readonly<numb
 maxmsp.addHandler('__analyseSweep', (threshold: Readonly<number> = -40): void => {
 	/*
 	Detect the dominant modes in an SPL test using the ___ algorithm.
-		See: <link.to.Julius.Caesar.Smith.III>
+		See: https://ccrma.stanford.edu/~jos/sasp/Peak_Detection_Steps_3.html
 	params:
 		threshold	minimum peak amplitude (db)
 	*/
 
-	let prev_amp = -Infinity
-	let prev_freq = 0.0
 	peaks = []
 	peaks_subset = []
-
-	// TODO: will this always assign the first entry as a peak? Should we instead
-	// start at i = 1?
-	// https://github.com/lewiswolf/drum-modal-feedback/issues/11
 	SPL_current.forEach((entry: Readonly<SPL[0]>, i: Readonly<number>) => {
-		if (i !== SPL_current.length - 1) {
+		if (i > 1 && i < SPL_current.length - 1) {
 			// typescript doesn't like complex for loops...
-			const next_amp = (SPL_current[i + 1] as NonNullable<SPL[0]>).amplitude
-			const next_freq = (SPL_current[i + 1] as NonNullable<SPL[0]>).frequency
+			const prev_entry = SPL_current[i - 1] as NonNullable<SPL[0]>
+			const next_entry = SPL_current[i + 1] as NonNullable<SPL[0]>
 			// logic
 			if (
-				entry.amplitude > prev_amp &&
-				entry.amplitude > next_amp &&
+				entry.amplitude > prev_entry.amplitude &&
+				entry.amplitude > next_entry.amplitude &&
 				(threshold === 0 || entry.amplitude > threshold)
 			) {
 				// Perform parabolic interpolation to better approximate the peak
 				// First calculate the frequency bin of the peak (-1.0 to 1.0)
-				// https://ccrma.stanford.edu/~jos/sasp/Peak_Detection_Steps_3.html
-				let freq = 0.5 * (prev_amp - next_amp)
-				freq = freq / (prev_amp - 2.0 * entry.amplitude + next_amp)
-
-				// Perform inerpolation of the amplitude
-				let amp = entry.amplitude - 0.25 * (prev_amp - next_amp) * freq
-
-				// Now convert the frequency bin to a frequency in hz
-				freq = entry.frequency + 0.5 * freq * (next_freq - prev_freq)
-
-				// Push the interpolated peak to the peaks array
-				peaks.push({ frequency: freq, amplitude: amp })
+				const bin =
+					(0.5 * (prev_entry.amplitude - next_entry.amplitude)) /
+					(prev_entry.amplitude - 2.0 * entry.amplitude + next_entry.amplitude)
+				peaks.push({
+					// Convert the frequency bin to a frequency in hz
+					frequency: entry.frequency + 0.5 * (next_entry.frequency - prev_entry.frequency) * bin,
+					// Perform interpolation of the amplitude
+					amplitude: entry.amplitude - 0.25 * (prev_entry.amplitude - next_entry.amplitude) * bin,
+				})
 			}
-			prev_amp = entry.amplitude
-			prev_freq = entry.frequency
 		}
 	})
-
 	// sort peaks by amplitude
 	peaks.sort((a, b) => b.amplitude - a.amplitude)
 	maxmsp.outletBang()
